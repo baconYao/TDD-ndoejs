52de95c2b70a3378a1569a9ae1bdc3efe71e5383
peiyao <peiyaochang@qnap.com>
2020-03-18 23:15:10 +0800

    Added the integration test of GET /todos endpoint

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -2,9 +2,22 @@ const request = require("supertest");
 const app = require("../../app");
 const newTodo = require("../mock-data/new-todo.json");
 
+/* Testing Node.js + Mongoose with an in-memory database
+ *    https://dev.to/paulasantamaria/testing-node-js-mongoose-with-an-in-memory-database-32np
+ * https://github.com/Hyllesen/express-tdd/tree/11_InMemoryDb
+ */ 
+
 const endpointUrl = "/todos/";
 
 describe(endpointUrl, () => {
+  it(`GET ${endpointUrl}`, async () => {
+    const response = await request(app).get(endpointUrl);
+
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.body)).toBeTruthy();
+    expect(response.body[0].title).toBeDefined();
+    expect(response.body[0].done).toBeDefined();
+  });
   it(`POST ${endpointUrl}`, async () => {
     const response = await request(app)
       .post(endpointUrl)
